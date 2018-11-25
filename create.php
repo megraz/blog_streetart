e749984f4601195d0d07b54120f961c5c58446b1
Egraz Marlene <marlene.egraz@gmail.com>
2018-11-25 19:51:17 +0100

    add home page

--- create.php
+++ create.php
@@ -3,9 +3,18 @@
     $categories = getAllCategories();
 ?>
 
-
 <?php include "header.php"; ?>
-    
+
+<?php 
+// isset = test le formulaire ; is_dir= verifie si la variable correspond à isset
+if (isset($_POST['filename'])){ //on utilise isset pr vérifier que le fichier existe//
+    //on récupère les données titre du fichier et on rajoute basename([], ".txt") pr supp le txt qui s'ecrit en plus//
+    $titre = htmlspecialchars(basename($_POST['filename'], ".txt")); 
+    $Images = htmlspecialchars(file_get_contents('post/'.$_POST['Images']));
+    $categorie = htmlspecialchars(file_get_contents('post/'.$_POST['categorie']));
+    $commentaire = htmlspecialchars(file_get_contents('post/'.$_POST['filename'])); // on recup le contenu et on y accède//
+    }
+?>
     <div class="container">
         <div class="row">
             <div class="col-md-3"></div>
